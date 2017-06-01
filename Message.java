ffa8035e0f62b7feddefb955fabbc49e20a25996
Fernando Rocha <fmrocha@student.dei.uc.pt>
2017-06-01 14:56:55 +0100

    Message updated

--- Message.java
+++ Message.java
@@ -2,18 +2,13 @@ import java.io.Serializable;
 import java.security.Key;
 
 public class Message implements Serializable {
-    private String data;
     private Key symmetric;
     private byte[] encryptedData;
 
 
-    public Message(String data, Key symmetric) {
-        this.data = data;
+    public Message(byte[] encryptedData, Key symmetric) {
+        this.encryptedData = encryptedData;
         this.symmetric = symmetric;
-    }
-
-    public String getData() {
-        return data;
     }
 
     public Key getSymmetric() {
